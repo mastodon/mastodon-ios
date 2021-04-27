d4552332b32d5504b14ed113d26aedd58ea37756
CMK <cirno.mainasuk@gmail.com>
2021-04-27 16:29:10 +0800

    chore: update CI script

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 sudo gem install cocoapods-keys
+
+# stub keys. DO NOT use in production
+pod keys set notification_endpoint "<endpoint>"
+pod keys set notification_endpoint_debug "<endpoint>"
+
 pod install
