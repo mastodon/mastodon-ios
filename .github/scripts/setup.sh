98f682d61a96fff3ebc3db8be7d97ac1f6bd3a3e
CMK <cirno.mainasuk@gmail.com>
2021-08-04 17:50:16 +0800

    chore: not run pod under CI root

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -3,7 +3,7 @@
 sudo gem install cocoapods-keys
 
 # stub keys. DO NOT use in production
-sudo pod keys set notification_endpoint "<endpoint>"
-sudo pod keys set notification_endpoint_debug "<endpoint>"
+pod keys set notification_endpoint "<endpoint>"
+pod keys set notification_endpoint_debug "<endpoint>"
 
-sudo pod install
+pod install
