6117c903f14508c7c7ef79e88a7ba42d3eb0b0f3
CMK <cirno.mainasuk@gmail.com>
2021-11-04 19:24:46 +0800

    fix: register may fail due to locale parameter issue

---
+++ Mastodon/Helper/MastodonLocalCode.swift
@@ -0,0 +1,12 @@
+//
+//  MastodonLocalCode.swift
+//  Mastodon
+//
+//  Created by Cirno MainasuK on 2021-11-4.
+//
+
+import Foundation
+
+// https://github.com/gunchleoc/mastodon/blob/ed6153b8f24d3a8f5a124cc95683bd1f20aec882/app/helpers/settings_helper.rb
+// last update 2021/11/4
+typealias MastodonLocalCode = [String: String]
