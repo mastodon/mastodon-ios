aa87340345080e3d5b004158a804473e36a34ee1
Jed Fox <git@jedfox.com>
2022-11-07 18:21:19 -0500

    set up translations for link labels

--- MastodonSDK/Sources/MastodonUI/Extension/MetaEntity+Accessibility.swift
+++ MastodonSDK/Sources/MastodonUI/Extension/MetaEntity+Accessibility.swift
@@ -11,12 +11,16 @@ extension Meta.Entity {
     var accessibilityCustomActionLabel: String? {
         switch meta {
         case .url(_, trimmed: _, url: let url, userInfo: _):
+            // TODO: i18n (a11y.meta_entity.url)
             return "Link: \(url)"
         case .hashtag(_, hashtag: let hashtag, userInfo: _):
+            // TODO: i18n (a11y.meta_entity.hashtag)
             return "Hashtag \(hashtag)"
         case .mention(_, mention: let mention, userInfo: _):
-            return "Show Profile: @\(mention)"
+            // TODO: i18n (a11y.meta_entity.mention)
+            return "Show Profile: \("@" + mention)"
         case .email(let email, userInfo: _):
+            // TODO: i18n (a11y.meta_entity.email)
             return "Email address: \(email)"
         // emoji are not actionable
         case .emoji:
