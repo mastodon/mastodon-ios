a36a303532e35b024d5990f3725a84700726e6c9
woxtu <woxtup@gmail.com>
2022-11-14 02:53:59 +0900

    Fix typos

--- MastodonSDK/Sources/MastodonCore/Service/AuthenticationService.swift
+++ MastodonSDK/Sources/MastodonCore/Service/AuthenticationService.swift
@@ -67,7 +67,7 @@ public final class AuthenticationService: NSObject {
             try mastodonAuthenticationFetchedResultsController.performFetch()
             mastodonAuthentications = mastodonAuthenticationFetchedResultsController.fetchedObjects?
                 .sorted(by: { $0.activedAt > $1.activedAt })
-                .compactMap { $0.asRecrod } ?? []
+                .compactMap { $0.asRecord } ?? []
         } catch {
             assertionFailure(error.localizedDescription)
         }
@@ -148,7 +148,7 @@ extension AuthenticationService: NSFetchedResultsControllerDelegate {
     
         mastodonAuthentications = mastodonAuthenticationFetchedResultsController.fetchedObjects?
             .sorted(by: { $0.activedAt > $1.activedAt })
-            .compactMap { $0.asRecrod } ?? []
+            .compactMap { $0.asRecord } ?? []
     }
     
 }
