c35a5cd4bfbd80f05c8b4d47eb1ec504064860d1
Jed Fox <git@jedfox.com>
2022-11-15 21:43:15 -0500

    Limit single media to a reasonable height unless in .plain

--- MastodonSDK/Sources/MastodonUI/View/Content/StatusView.swift
+++ MastodonSDK/Sources/MastodonUI/View/Content/StatusView.swift
@@ -545,6 +545,13 @@ extension StatusView {
         return frame.width
     }
 
+    public var contentMaxLayoutHeight: CGFloat {
+        if style == .plain {
+            return .greatestFiniteMagnitude
+        }
+        return 512
+    }
+
 }
 
 extension StatusView {
