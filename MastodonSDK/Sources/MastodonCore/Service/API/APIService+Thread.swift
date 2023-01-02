843eeed616817ab218e7dab644aa2f050bceb688
jinsu kim <jinsukim21@gmail.com>
2023-01-02 00:26:15 -0800

    Leave data loading to ThreadViewModel

--- MastodonSDK/Sources/MastodonCore/Service/API/APIService+Thread.swift
+++ MastodonSDK/Sources/MastodonCore/Service/API/APIService+Thread.swift
@@ -81,31 +81,31 @@ extension APIService {
             )
         }
         
-        let responseTwo = try await Mastodon.API.Statuses.statusContext(
-            session: session,
-            domain: domain,
-            statusID: statusID,
-            authorization: authorization
-        ).singleOutput()
-        
-        try await managedObjectContext.performChanges {
-            let me = authenticationBox.authenticationRecord.object(in: managedObjectContext)?.user
-            let value = responseTwo.value.ancestors + responseTwo.value.descendants
-            
-            for entity in value {
-                _ = Persistence.Status.createOrMerge(
-                    in: managedObjectContext,
-                    context: Persistence.Status.PersistContext(
-                        domain: domain,
-                        entity: entity,
-                        me: me,
-                        statusCache: nil,
-                        userCache: nil,
-                        networkDate: responseTwo.networkDate
-                    )
-                )
-            }
-        }
+//        let responseTwo = try await Mastodon.API.Statuses.statusContext(
+//            session: session,
+//            domain: domain,
+//            statusID: statusID,
+//            authorization: authorization
+//        ).singleOutput()
+//        
+//        try await managedObjectContext.performChanges {
+//            let me = authenticationBox.authenticationRecord.object(in: managedObjectContext)?.user
+//            let value = responseTwo.value.ancestors + responseTwo.value.descendants
+//            
+//            for entity in value {
+//                _ = Persistence.Status.createOrMerge(
+//                    in: managedObjectContext,
+//                    context: Persistence.Status.PersistContext(
+//                        domain: domain,
+//                        entity: entity,
+//                        me: me,
+//                        statusCache: nil,
+//                        userCache: nil,
+//                        networkDate: responseTwo.networkDate
+//                    )
+//                )
+//            }
+//        }
                 
         var result: Status?
         try await managedObjectContext.perform {
