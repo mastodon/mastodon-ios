7beda3ab7ebf7fec38a8757eb3926648e3f1aa1f
Jed Fox <git@jedfox.com>
2023-01-06 11:13:28 -0500

    initial alt text detection

--- MastodonSDK/Sources/MastodonUI/Scene/ComposeContent/Attachment/AttachmentDescriptionComposerView.swift
+++ MastodonSDK/Sources/MastodonUI/Scene/ComposeContent/Attachment/AttachmentDescriptionComposerView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import VisionKit
 
 struct AttachmentDescriptionComposerView: View {
     internal init(prompt: String, thumbnail: UIImage, description: Binding<String>) {
@@ -13,6 +14,7 @@ struct AttachmentDescriptionComposerView: View {
         self.thumbnail = thumbnail
         self._savedDescription = description
         self._description = State(initialValue: description.wrappedValue)
+        self._analysis = StateObject(wrappedValue: ImageAnalyzerModel(image: thumbnail))
     }
 
     let prompt: String
@@ -21,6 +23,7 @@ struct AttachmentDescriptionComposerView: View {
 
     @Environment(\.dismiss) private var dismiss
     @State private var description: String
+    @StateObject private var analysis: ImageAnalyzerModel
 
     var body: some View {
         NavigationView {
@@ -33,6 +36,26 @@ struct AttachmentDescriptionComposerView: View {
                             .aspectRatio(contentMode: .fit)
                             .frame(maxHeight: 200)
                             .cornerRadius(4)
+                            .overlay(alignment: .bottomTrailing) {
+                                if let transcript = analysis.transcript, !description.contains(transcript) {
+                                    Button {
+                                        if description.isEmpty {
+                                            description = transcript
+                                        } else {
+                                            description += "\n\n" + transcript
+                                        }
+                                    } label: {
+                                        Label("Use Detected Text", systemImage: "text.viewfinder")
+                                        .foregroundColor(.black)
+                                    }
+                                    .buttonStyle(.borderedProminent)
+                                    .buttonBorderShape(.capsule)
+                                    .tint(.white)
+                                    .padding()
+                                    .transition(.opacity)
+                                }
+                            }
+                            .animation(.default, value: analysis.transcript)
                         Spacer(minLength: 0)
                     }
                 })
@@ -53,5 +76,28 @@ struct AttachmentDescriptionComposerView: View {
                 }
             }
         }
+    }
+}
+
+private class ImageAnalyzerModel: ObservableObject {
+    let image: UIImage
+    
+    @Published var transcript: String?
+    
+    private var analysisTask: Task<Void, Error>?
+
+    init(image: UIImage) {
+        self.image = image
+        
+        guard #available(iOS 16, *), ImageAnalyzer.isSupported else { return }
+        let analyzer = ImageAnalyzer()
+        analysisTask = Task.detached(priority: .high) { [weak self] in
+            let result = try await analyzer.analyze(image, configuration: .init(.text))
+            self?.transcript = result.transcript
+        }
+    }
+    
+    deinit {
+        analysisTask?.cancel()
     }
 }
