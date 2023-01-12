4b44007a471584a5b4244d72d655517502ff8732
Jed Fox <git@jedfox.com>
2023-01-12 09:07:45 -0500

    Fix accessibility label for posts with custom emoji

--- MastodonSDK/Sources/MastodonUI/View/Content/StatusView+ViewModel.swift
+++ MastodonSDK/Sources/MastodonUI/View/Content/StatusView+ViewModel.swift
@@ -751,7 +751,7 @@ extension StatusView.ViewModel {
             }
 
             if isContentReveal {
-                strings.append(content?.string)
+                strings.append(statusView.contentMetaText.backedString)
             }
             
             return strings.compactMap { $0 }.joined(separator: ", ")
@@ -769,6 +769,7 @@ extension StatusView.ViewModel {
         contentAccessibilityLabel
             .sink { contentAccessibilityLabel in
                 statusView.spoilerOverlayView.accessibilityLabel = contentAccessibilityLabel
+                statusView.contentMetaText.textView.accessibilityLabel = contentAccessibilityLabel
             }
             .store(in: &disposeBag)
 
