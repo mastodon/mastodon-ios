faec1cb9a3d826d473b0b33a44031136c843b406
Nathan Mattes <hallo@bullenscheisse.de>
2023-01-29 16:13:46 +0100

    Show different buttons when editing a status (IOS-2)

--- Mastodon/Scene/Root/MainTab/MainTabBarController.swift
+++ Mastodon/Scene/Root/MainTab/MainTabBarController.swift
@@ -386,9 +386,10 @@ extension MainTabBarController {
         let composeViewModel = ComposeViewModel(
             context: context,
             authContext: authContext,
+            composeContext: .composeStatus,
             destination: .topLevel
         )
-        _ = coordinator.present(scene: .compose(viewModel: composeViewModel), from: nil, transition: .modal(animated: true, completion: nil))
+        _ = coordinator.present(scene: .compose(viewModel: composeViewModel), transition: .modal(animated: true, completion: nil))
     }
     
     private func touchedTab(by sender: UIGestureRecognizer) -> Tab? {
@@ -815,6 +816,7 @@ extension MainTabBarController {
         let composeViewModel = ComposeViewModel(
             context: context,
             authContext: authContext,
+            composeContext: .composeStatus,
             destination: .topLevel
         )
         _ = coordinator.present(scene: .compose(viewModel: composeViewModel), from: nil, transition: .modal(animated: true, completion: nil))
