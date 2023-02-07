d933f2d0cb0f1b6208cbbd86dbcfdf9dd8bba566
Jed Fox <git@jedfox.com>
2023-02-06 22:08:38 -0500

    Fix sizing of userView in search history

--- Mastodon/Scene/Search/SearchDetail/SearchHistory/Cell/SearchHistoryUserCollectionViewCell.swift
+++ Mastodon/Scene/Search/SearchDetail/SearchHistory/Cell/SearchHistoryUserCollectionViewCell.swift
@@ -51,7 +51,7 @@ extension SearchHistoryUserCollectionViewCell {
         NSLayoutConstraint.activate([
             userView.topAnchor.constraint(equalTo: contentView.topAnchor),
             userView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            userView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 16),
+            contentView.trailingAnchor.constraint(equalTo: userView.trailingAnchor, constant: 16),
             userView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
         ])
     }
