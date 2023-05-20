7c94378b9b81ecf8eb51d74f8231d19f280e8993
Nathan Mattes <hallo@bullenscheisse.de>
2023-05-20 11:30:50 +0200

    Make update-script speak greek (#1038)

--- Localization/StringsConvertor/Sources/StringsConvertor/main.swift
+++ Localization/StringsConvertor/Sources/StringsConvertor/main.swift
@@ -74,6 +74,7 @@ private func map(language: String) -> String? {
     case "th.lproj":        return "th"         // Thai
     case "tr.lproj":        return "tr"         // Turkish
     case "vi.lproj":        return "vi"         // Vietnamese
+    case "el.lproj":        return "el"         // Vietnamese
     default:                return nil
     }
 }
