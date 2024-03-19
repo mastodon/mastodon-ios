27353a3bdb0a48d71be6556e4bf1a4a7c0c8346f
woxtu <woxtup@gmail.com>
2024-03-20 00:16:42 +0900

    Change user agent (#1250)

--- MastodonSDK/Sources/MastodonCore/Service/API/APIService.swift
+++ MastodonSDK/Sources/MastodonCore/Service/API/APIService.swift
@@ -31,8 +31,12 @@ public final class APIService {
     
     public init(backgroundManagedObjectContext: NSManagedObjectContext) {
         self.backgroundManagedObjectContext = backgroundManagedObjectContext
-        self.session = URLSession(configuration: .default)
-        
+
+        let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = ["User-Agent" : "mastodon-ios/" + appVersion]
+        self.session = URLSession(configuration: configuration)
+
         // setup cache. 10MB RAM + 50MB Disk
         URLCache.shared = URLCache(memoryCapacity: 10 * 1024 * 1024, diskCapacity: 50 * 1024 * 1024, diskPath: nil)
         
