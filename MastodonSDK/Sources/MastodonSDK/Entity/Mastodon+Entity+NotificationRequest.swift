b119a6db1fa9e1810abf3cb18acf37040781d243
Nathan Mattes <hallo@bullenscheisse.de>
2024-07-09 18:29:05 +0200

    Use correct properties for account (IOS-241)

--- MastodonSDK/Sources/MastodonSDK/Entity/Mastodon+Entity+NotificationRequest.swift
+++ MastodonSDK/Sources/MastodonSDK/Entity/Mastodon+Entity+NotificationRequest.swift
@@ -7,15 +7,15 @@ extension Mastodon.Entity {
         public let id: String
         public let createdAt: Date
         public let updatedAt: Date
-        public let fromAccount: Mastodon.Entity.Account
-        public let notificationsCount: Int
+        public let account: Mastodon.Entity.Account
+        public let notificationsCount: String // contains an `Int`
         public let lastStatus: Mastodon.Entity.Status?
 
         enum CodingKeys: String, CodingKey {
             case id = "id"
             case createdAt = "created_at"
             case updatedAt = "updated_at"
-            case fromAccount = "from_account"
+            case account = "account"
             case notificationsCount = "notifications_count"
             case lastStatus = "last_status"
         }
