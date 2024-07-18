b252536613a3876ab30ee880f05d605c9f46d637
Marcus Kida <marcus.kida@bearologics.com>
2024-07-18 11:53:47 +0200

    Fix MastodonIntent is unable to load data due to missing credentials

--- MastodonIntent/IntentHandler.swift
+++ MastodonIntent/IntentHandler.swift
@@ -6,12 +6,13 @@
 //
 
 import Intents
+import MastodonCore
 
 class IntentHandler: INExtension {
     
     override func handler(for intent: INIntent) -> Any {
-        // This is the default implementation.  If you want different objects to handle different intents,
-        // you can override this and return the handler you want for that particular intent.
+        AuthenticationServiceProvider.shared.restore()
+
         switch intent {
         case is SendPostIntent:
             return SendPostIntentHandler()
