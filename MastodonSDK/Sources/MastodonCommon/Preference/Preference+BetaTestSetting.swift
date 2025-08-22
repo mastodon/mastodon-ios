b2e4ea2e64da03f946e01eb842e1f20b9dc397b5
whattherestimefor <shannon@joinmastodon.org>
2025-08-22 14:16:30 +0100

    Turn on new timeline by default for TestFlight builds

--- MastodonSDK/Sources/MastodonCommon/Preference/Preference+BetaTestSetting.swift
+++ MastodonSDK/Sources/MastodonCommon/Preference/Preference+BetaTestSetting.swift
@@ -48,7 +48,7 @@ extension UserDefaults {
     
     @objc public dynamic var testNewHomeTimeline: Bool {
         get {
-            register(defaults: [#function: false])
+            register(defaults: [#function: true])
             return bool(forKey: #function) && UserDefaults.isDebugOrTestflightOrSimulator
         }
         set { self[#function] = newValue }
