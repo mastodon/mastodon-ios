a6b5bbac7ff719d3c86ba569ab8301a0d9a11cfe
shannon <shannon@joinmastodon.org>
2025-10-21 07:56:55 +0100

    Add a dedicated audio player
    Fixes IOS-569

--- Mastodon/In Progress New Layout and Datamodel/Common Components/Views/TimelineRowViews/Molecules/MediaAttachmentView.swift
+++ Mastodon/In Progress New Layout and Datamodel/Common Components/Views/TimelineRowViews/Molecules/MediaAttachmentView.swift
@@ -185,9 +185,11 @@ extension MediaAttachment {
                 ImageGridView(mediaPreviewableViewController: actionHandler.mediaPreviewableViewController)
                     .environment(ImageGalleryViewModel(imageAttachments: attachments, altTextTranslations: altTextTranslations, actionHandler: actionHandler))
             }
-        case .audio, .gifv, .video:
+        case .audio:
+            AudioPlayerView(media: self)
+        case .gifv, .video:
             ConcealableMediaAttachmentView() {
-                PlayerView(media: self, actionHandler: actionHandler)
+                VideoPlayerView(media: self, actionHandler: actionHandler)
             }
         case .openInBrowser(let url):
             Button {
@@ -461,23 +463,167 @@ class ImageGalleryViewModel {
     }
 }
 
-struct PlayerView: View {
+struct AudioPlayerView: View {
     let media: MediaAttachment
+    let url: URL
+    @StateObject private var playerObserver = PlayerObserver()
+    @State private var isDimmingButton = false
+    
+    init?(media: MediaAttachment) {
+        switch media {
+        case .audio:
+            break
+        default:
+            return nil
+        }
+        self.media = media
+        guard let url = media.attachmentInfo?.url else { return nil }
+        self.url = url
+    }
+    
+    var body: some View {
+        VStack {
+            // BUTTONS
+            HStack {
+                Button {
+                    // jump 5 sec backwards
+                    let currentTimeInSeconds = playerObserver.currentTimeInSeconds
+                    let destination = max(0, currentTimeInSeconds - 5)
+                    playerObserver.jump(to: destination)
+                } label: {
+                    Image(systemName: "5.arrow.trianglehead.counterclockwise")
+                }
+                
+                Spacer()
+                
+                switch playerObserver.playingState {
+                case .paused:
+                    Button {
+                        playerObserver.didPressPlay()
+                    } label: {
+                        Image(systemName: "play.fill")
+                    }
+                case .waitingToPlayAtSpecifiedRate:
+                    Button {
+                        playerObserver.didPressPause()
+                        isDimmingButton = false
+                    } label: {
+                        Image(systemName: "pause.fill")
+                            .opacity(isDimmingButton ? 1.0 : 0.3)
+                            .onAppear {
+                                withAnimation(
+                                    Animation.easeInOut(duration: 1.0)
+                                        .repeatForever(autoreverses: true)
+                                ) {
+                                    isDimmingButton = true
+                                }
+                            }
+                    }
+                case .playing:
+                    Button {
+                        playerObserver.didPressPause()
+                    } label: {
+                        Image(systemName: "pause.fill")
+                    }
+                @unknown default:
+                    EmptyView()
+                }
+                
+                Spacer()
+                
+                Button {
+                    // jump 5 sec forward
+                    guard let maxTime = playerObserver.totalSeconds else { return }
+                    let currentTimeInSeconds = playerObserver.currentTimeInSeconds
+                    let destination = min(maxTime, currentTimeInSeconds + 5)
+                    playerObserver.jump(to: destination)
+                } label: {
+                    Image(systemName: "5.arrow.trianglehead.clockwise")
+                }
+            }
+            .foregroundStyle(.primary)
+            .font(.title)
+            .frame(width: 200)
+            .padding()
+            
+            // SLIDER
+            Slider(
+                value: Binding(
+                    get: {
+                        playerObserver.currentTimeInSeconds
+                    },
+                    set: { newValue in
+                        playerObserver.jump(to: newValue)
+                    }),
+                in: 0...(playerObserver.totalSeconds ?? 1),
+                label: {
+                    Text("Audio timestamp")
+                },
+                minimumValueLabel: {
+                    Text(timeLabel(fromSeconds: playerObserver.currentTimeInSeconds))
+                        .font(.system(.caption, design: .monospaced))
+                },
+                maximumValueLabel: {
+                    Text(timeLabel(fromSeconds: playerObserver.totalSeconds))
+                        .font(.system(.caption, design: .monospaced))
+                }
+            )
+            .accentColor(.secondary)
+            .tint(.secondary)
+            
+            // ALT TEXT
+            if let altText = media.attachmentInfo?.basicData.altText {
+                Text(altText)
+                    .foregroundStyle(.primary)
+                    .font(.subheadline)
+                    .italic()
+                    .multilineTextAlignment(.center)
+            }
+        }
+        .padding()
+        .background {
+            MastodonSecondaryBackground(fillInDarkModeOnly: true)
+        }
+        .environment(\.colorScheme, .dark)
+        .onAppear() {
+            playerObserver.setPlayer(withAsset: AVAsset(url: url))
+            playerObserver.startObserving(shouldLoop: false)
+        }
+        .onDisappear() {
+            playerObserver.didPressPause()
+        }
+    }
+    
+    func timeLabel(fromSeconds seconds: Double?) -> String {
+        if let seconds {
+            let sec = Int(seconds) % 60
+            let min = Int(seconds) / 60
+            return String(format: "%02d:%02d", min, sec)
+        } else {
+            return "--"
+        }
+    }
+}
+
+struct VideoPlayerView: View {
+    let media: MediaAttachment
+    let url: URL
     @Environment(ContentConcealViewModel.self) private var contentConcealViewModel
-    @StateObject var playerObserver = VideoPlayerObserver()
-    let player: AVPlayer?
+    @StateObject var playerObserver = PlayerObserver()
     let actionHandler: MastodonPostMenuActionHandler
     @State var waitingToShowFullSize = false
     
-    init(media: MediaAttachment, actionHandler: MastodonPostMenuActionHandler) {
-        self.media = media
-        self.actionHandler = actionHandler
-        
-        if let attachmentInfo = media.attachmentInfo, let url = attachmentInfo.url {
-            self.player = AVPlayer(url: url)
-        } else {
-            self.player = nil
+    init?(media: MediaAttachment, actionHandler: MastodonPostMenuActionHandler) {
+        switch media {
+        case .video, .gifv:
+            break
+        default:
+            return nil
         }
+        guard let attachmentInfo = media.attachmentInfo, let url = attachmentInfo.url else { return nil }
+        self.media = media
+        self.url = url
+        self.actionHandler = actionHandler
     }
     
     var body: some View {
@@ -488,20 +634,23 @@ struct PlayerView: View {
                     .scaledToFill()
             }
             
-            VideoPlayer(player: playerObserver.player)
-                .background {
-                    if waitingToShowFullSize {
-                        FrameReader() { frame in
-                            playerObserver.mostRecentFrameInScreenCoordinates = frame
-                            if waitingToShowFullSize {
-                                waitingToShowFullSize = false
-                                Task { @MainActor in
-                                    showFullSize()
+            if let player = playerObserver.getPlayer() {
+                VideoPlayer(player: player)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background {
+                        if waitingToShowFullSize {
+                            FrameReader() { frame in
+                                playerObserver.mostRecentFrameInScreenCoordinates = frame
+                                if waitingToShowFullSize {
+                                    waitingToShowFullSize = false
+                                    Task { @MainActor in
+                                        showFullSize()
+                                    }
                                 }
                             }
                         }
                     }
-                }
+            }
         }
         .overlay {
             ZStack {
@@ -513,26 +662,34 @@ struct PlayerView: View {
                 }
                 .buttonStyle(.borderless)
                 
-                if shouldShowPlayButton && !playerObserver.isPlaying {
-                    Button {
-                        playerObserver.player?.play()
-                    } label: {
-                        Image(systemName: "play.fill")
-                            .font(.title2)
-                            .padding(EdgeInsets(top: standardPadding, leading: doublePadding, bottom: standardPadding, trailing: doublePadding))
-                            .background() {
-                                Capsule()
-                                    .fill(.ultraThinMaterial)
-                            }
+                if shouldShowPlayButton {
+                    switch playerObserver.playingState {
+                    case .paused:
+                        Button {
+                            playerObserver.didPressPlay()
+                        } label: {
+                            Image(systemName: "play.fill")
+                                .font(.title2)
+                                .padding(EdgeInsets(top: standardPadding, leading: doublePadding, bottom: standardPadding, trailing: doublePadding))
+                                .background() {
+                                    Capsule()
+                                        .fill(.ultraThinMaterial)
+                                }
+                        }
+                        .buttonStyle(.borderless)
+                    case .waitingToPlayAtSpecifiedRate:
+                        ProgressView().progressViewStyle(.circular)
+                    case .playing:
+                        EmptyView()
+                    @unknown default:
+                        EmptyView()
                     }
-                    .buttonStyle(.borderless)
                 }
             }
         }
         .onAppear() {
-            if let player, player != playerObserver.player {
-                playerObserver.startObserving(player: player, shouldLoop: shouldLoop)
-            }
+            self.playerObserver.setPlayer(withAsset: AVAsset(url: url))
+            playerObserver.startObserving(shouldLoop: shouldLoop)
             if let attachmentInfo = media.attachmentInfo, let url = attachmentInfo.url, let blurhash = attachmentInfo.blurhash, let size = attachmentInfo.size {
                 Task {
                     playerObserver.blurImage = try? await BlurhashImageCacheService.shared.image(blurhash: blurhash, size: size, url: url.absoluteString).singleOutput()
@@ -540,7 +697,7 @@ struct PlayerView: View {
             }
         }
         .onDisappear() {
-            playerObserver.player?.pause()
+            playerObserver.didPressPause()
         }
     }
     
@@ -563,7 +720,7 @@ struct PlayerView: View {
     }
     
     func showFullSize() {
-        player?.pause()
+        playerObserver.didPressPause()
         guard let _legacyEntity = media.attachmentInfo?._legacyEntity, let previewableViewController = actionHandler.mediaPreviewableViewController else { return }
         let previewItem: MediaPreviewViewModel.PreviewItem = .attachments([_legacyEntity], initialIndex: 0, altTexts: [media.attachmentInfo?.basicData.altText ?? ""])
         let mediaPreviewTransitionItem: MediaPreviewTransitionItem = {
@@ -607,37 +764,104 @@ extension MediaAttachment {
     }
 }
 
-class VideoPlayerObserver: ObservableObject {
-    @Published var isPlaying: Bool = false
-    @Published private(set) var player: AVPlayer?
+class PlayerObserver: ObservableObject {
+    @Published var playingState: AVPlayer.TimeControlStatus = .paused
+    @Published var totalSeconds: Double?
+    @Published var currentTimeInSeconds: Double = 0.0
     @Published var blurImage: UIImage? = nil
-    private var cancellable: AnyCancellable?
     var mostRecentFrameInScreenCoordinates = CGRect(x: 50, y: 50, width: 50, height: 50)
     
-    func startObserving(player: AVPlayer, shouldLoop: Bool) {
-        self.cancellable?.cancel()
-        self.player?.pause()
-        self.player = player
-        self.cancellable = player.publisher(for: \.rate, options: [.initial, .new])
-            .receive(on: DispatchQueue.main)
-            .map { $0 != 0 }
-            .assign(to: \.isPlaying, on: self)
+    private var player: AVPlayer?
+    private var timeObserverToken: Any?
+    private var playerStatusSubscription: AnyCancellable?
+    private var playShouldSeekToStart = false
+    
+    func setPlayer(withAsset asset: AVAsset) {
+        guard self.player == nil else { return }
+        let _player = AVPlayer(playerItem: AVPlayerItem(asset: asset))
+        self.player = _player
         
-        if shouldLoop {
-            NotificationCenter.default.addObserver(
-                forName: .AVPlayerItemDidPlayToEndTime,
-                object: player.currentItem,
-                queue: .main
-            ) { [weak self] _ in
+        func secondsFromDuration(_ duration: CMTime?) -> Double? {
+            if let seconds = duration?.seconds, seconds > 0, seconds.isFinite, !seconds.isNaN {
+                return seconds
+            } else {
+                return nil
+            }
+        }
+        
+        totalSeconds = secondsFromDuration(_player.currentItem?.duration)
+        
+        Task {
+            let duration = try await asset.load(.duration)
+            totalSeconds = secondsFromDuration(duration)
+        }
+    }
+    
+    func startObserving(shouldLoop: Bool) {
+        guard let player else { assertionFailure(); return }
+        player.pause()
+        self.playerStatusSubscription?.cancel()
+        self.playerStatusSubscription = player.publisher(for: \.timeControlStatus, options: [.initial, .new])
+            .receive(on: DispatchQueue.main)
+            .map { $0 }
+            .assign(to: \.playingState, on: self)
+        
+        if timeObserverToken == nil {
+            let interval = CMTime(seconds: 0.5, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+            timeObserverToken = player.addPeriodicTimeObserver(forInterval: interval, queue: .main) { [weak self] time in
+                self?.totalSeconds = player.currentItem?.duration.seconds
+                self?.currentTimeInSeconds = time.seconds
+            }
+        }
+        
+        NotificationCenter.default.addObserver(
+            forName: .AVPlayerItemDidPlayToEndTime,
+            object: player.currentItem,
+            queue: .main
+        ) { [weak self] _ in
+            if shouldLoop {
                 self?.player?.seek(to: .zero)
                 self?.player?.play()
+            } else {
+                self?.playShouldSeekToStart = true
             }
         }
     }
     
     deinit {
         NotificationCenter.default.removeObserver(self)
-        self.cancellable?.cancel()
+        self.playerStatusSubscription?.cancel()
         player?.pause()
+        if let timeObserverToken {
+            player?.removeTimeObserver(timeObserverToken)
+        }
+    }
+    
+    func didPressPlay() {
+        if playShouldSeekToStart {
+            player?.seek(to: .zero)
+            playShouldSeekToStart = false
+        }
+        player?.play()
+    }
+    
+    func didPressPause() {
+        player?.pause()
+    }
+    
+    func jump(to newTime: Double) {
+        if let item = player?.currentItem {
+            let scale = item.duration.timescale
+            let time = CMTime(seconds: newTime, preferredTimescale: scale)
+            player?.seek(to: time) { [weak self] finished in
+                guard let self, let totalSeconds else { return }
+                self.currentTimeInSeconds = item.currentTime().seconds
+                self.playShouldSeekToStart = time.seconds >= totalSeconds
+            }
+        }
+    }
+    
+    func getPlayer() -> AVPlayer? {
+        return player
     }
 }
