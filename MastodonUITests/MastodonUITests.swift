3af3c9c33eaeb10f6ef51d116f55defdf5068628
Clare So <1740517+clarmso@users.noreply.github.com>
2025-11-02 00:33:14 -0400

    Run smoke test on multiple device to ensure app can start for sure.

--- MastodonUITests/MastodonUITests.swift
+++ MastodonUITests/MastodonUITests.swift
@@ -26,6 +26,11 @@ class MastodonUITests: XCTestCase {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
+        XCTAssertTrue(app.navigationBars["Mastodon.WelcomeView"].exists)
+        XCTAssertTrue(app.buttons["Join mastodon.social"].isHittable)
+        XCTAssertTrue(app.buttons["Pick another server"].isHittable)
+        XCTAssertTrue(app.buttons["Learn more"].isHittable)
+        XCTAssertTrue(app.buttons["Log In"].isHittable)
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
