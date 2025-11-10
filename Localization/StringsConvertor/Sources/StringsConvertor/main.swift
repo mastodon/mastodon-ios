c619f8426d2ed271d916d5bfec83033b9d572d72
shannon <shannon@joinmastodon.org>
2025-11-10 16:59:37 +0100

    Add missing languages to the StringsConvertor mapping

--- Localization/StringsConvertor/Sources/StringsConvertor/main.swift
+++ Localization/StringsConvertor/Sources/StringsConvertor/main.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-// conver i18n JSON templates to strings files
+// convert i18n JSON templates to strings files
 private func convert(from inputDirectoryURL: URL, to outputDirectory: URL) {
     do {
         let inputLanguageDirectoryURLs = try FileManager.default.contentsOfDirectory(
@@ -44,35 +44,57 @@ private func convert(from inputDirectoryURL: URL, to outputDirectory: URL) {
 private func map(language: String) -> String? {
     switch language {
     case "Base.lproj":      return "Base"
+    case "an.lproj":        return "an"         // Aragonese
     case "ar.lproj":        return "ar"         // Arabic
-    case "eu.lproj":        return "eu"         // Basque
+    case "be.lproj":        return "be"         // Belarussian
     case "ca.lproj":        return "ca"         // Catalan
-    case "zh-Hans.lproj":   return "zh-Hans"    // Chinese Simplified
-    case "zh-Hant.lproj":   return "zh-Hant"    // Chinese Traditional
+    case "ckb.lproj":       return "ckb"        // Sorani (Kurdish)
     case "cs.lproj":        return "cs"         // Czech
-    case "nl.lproj":        return "nl"         // Dutch
-    case "en.lproj":        return "en"
+    case "cy.lproj":        return "cy"         // Welsh
+    case "da.lproj":        return "da"         // Danish
+    case "de.lproj":        return "de"         // German
+    case "el.lproj":        return "el"         // Greek
+    case "en.lproj":        return "en"         // English
+    case "en-US.lproj":     return "en-US"      // English (US)
+    case "es.lproj":        return "es"         // Spanish
+    case "es_AR.lproj":     return "es-AR"      // Spanish (Argentina)
+    case "et.lproj":        return "et"         // Estonian
+    case "eu.lproj":        return "eu"         // Basque
     case "fi.lproj":        return "fi"         // Finnish
     case "fr.lproj":        return "fr"         // French
+    case "gd.lproj":        return "gd"         // Scottish Gaelic
     case "gl.lproj":        return "gl"         // Galician
-    case "de.lproj":        return "de"         // German
+    case "he.lproj":        return "he"         // Hebrew
+    case "hi.lproj":        return "hi"         // Hindi
+    case "hu.lproj":        return "hu"         // Hungarian
+    case "hy.lproj":        return "hy"         // Armenian
+    case "hy-AM.lproj":     return "hy-AM"      // Armenian (Armenia)
+    case "id.lproj":        return "id"         // Indonesian
+    case "is.lproj":        return "is"         // Icelandic
     case "it.lproj":        return "it"         // Italian
     case "ja.lproj":        return "ja"         // Japanese
     case "kab.lproj":       return "kab"        // Kabyle
     case "kmr.lproj":       return "ku"         // Kurmanji (Kurdish) [intent mapping]
+    case "ko.lproj":        return "ko"         // Korean
+    case "lt.lproj":        return "lt"         // Lithuanian
+    case "lv.lproj":        return "lv"         // Latvian
+    case "my.lproj":        return "my"         // Burmese
+    case "nl.lproj":        return "nl"         // Dutch
+    case "pl.lproj":        return "pl"         // Polish
+    case "pt.lproj":        return "pt"         // Portuguese
+    case "pt-BR.lproj":     return "pt-BR"      // Portuguese (Brazil)
+    case "ro.lproj":        return "ro"         // Romanian
     case "ru.lproj":        return "ru"         // Russian
-    case "gd.lproj":        return "gd"         // Scottish Gaelic
+    case "si.lproj":        return "si"         // Sinhala
     case "sl.lproj":        return "sl"         // Slovenian
-    case "ckb.lproj":       return "ckb"        // Sorani (Kurdish)
-    case "es.lproj":        return "es"         // Spanish
-    case "es_AR.lproj":     return "es-AR"      // Spanish, Argentina
     case "sv.lproj":        return "sv"         // Swedish
     case "th.lproj":        return "th"         // Thai
     case "tr.lproj":        return "tr"         // Turkish
+    case "uk.lproj":        return "uk"         // Ukranian
     case "vi.lproj":        return "vi"         // Vietnamese
-    case "el.lproj":        return "el"         // Greek
-    case "be.lproj":        return "be"         // Belarussian
-    case "hy.lproj":        return "hy"         // Armenian
+    case "zh-Hans.lproj":   return "zh-Hans"    // Chinese Simplified
+    case "zh-Hant.lproj":   return "zh-Hant"    // Chinese Traditional
+  
     default:                return nil
     }
 }
