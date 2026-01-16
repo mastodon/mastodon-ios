631e363e8177c75a4a4f948e2ed61a6237dd1dd5
Jack Alto <384288+aokj4ck@users.noreply.github.com>
2026-01-16 10:09:23 -0500

    Fix MastodonSDK package tests (#1498)
    * Fix MastodonSDKTests sources (tests target still failing to test)
    
    * Improve MastodonSDKTests dependency linking, L10n errors remain
    
    * Fix package errors
    
    * Move CoreDataStack and MastodonLocalization dependencies to MastodonSDK to fix build warning
    
    * Remove duplicated and non-existent AppStoreSnapshotTestPlan\ copy.xctestplan reference
    
    * Restore MastodonSDK.xctestplan to main project Mastodon iOS build scheme for Testing
    
    * Fix typo in testCreateAnApplication

--- MastodonSDK/Package.swift
+++ MastodonSDK/Package.swift
@@ -103,7 +103,9 @@ let package = Package(
             name: "MastodonSDK",
             dependencies: [
                 .product(name: "NIOHTTP1", package: "swift-nio"),
-                "MastodonCommon"
+                "MastodonCommon",
+                "CoreDataStack",
+                "MastodonLocalization"
             ]
         ),
         .target(
