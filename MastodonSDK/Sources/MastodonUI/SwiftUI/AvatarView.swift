250b9a23fb22269fdee3f858b22f80279c6e39c2
shannon <shannon@joinmastodon.org>
2026-02-17 12:31:53 +0100

    Styling improvements to AvatarView
    Include a background in case the image has an alpha channel.
    
    Use the device scale to get a single pixel border for the separator style.
    
    Add a pixel border + separator option for use in overlapping avatar stacks.

--- MastodonSDK/Sources/MastodonUI/SwiftUI/AvatarView.swift
+++ MastodonSDK/Sources/MastodonUI/SwiftUI/AvatarView.swift
@@ -27,6 +27,7 @@ public struct AvatarView: View {
     var sizeTiny = AvatarSize.tiny
     
     @State var isNavigating: Bool = false
+    @Environment(\.displayScale) var displayScale
     
     public enum Size {
         case extraExtraLarge
@@ -51,17 +52,25 @@ public struct AvatarView: View {
         }
     }
     
+    public enum BorderStyle {
+        case backgroundMatching
+        case separator
+        case both
+    }
+    
     public enum AvatarSource {
         case url(URL?)
         case local(Image)
     }
     
     let size: Size
+    let borderStyle: BorderStyle?
     let avatarSource: AvatarSource?
     let goToProfile: (() async throws -> ())?
     
-    public init(size: Size, avatarSource: AvatarSource?, goToProfile: (() async throws -> ())?) {
+    public init(size: Size, borderStyle: BorderStyle? = nil, avatarSource: AvatarSource?, goToProfile: (() async throws -> ())?) {
         self.size = size
+        self.borderStyle = borderStyle
         self.avatarSource = avatarSource
         self.goToProfile = goToProfile
     }
@@ -82,50 +91,33 @@ public struct AvatarView: View {
     
     public var body: some View {
         ZStack {
-            if let avatarSource {
-                switch avatarSource {
-                case .url(let url):
-                    if let url {
-                        WebImage(
-                            url: url,
-                            content: { image in
-                                image.resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .clipShape(avatarShape)
-                            },
-                            placeholder: {
-                                avatarShape
-                                    .foregroundStyle(
-                                        Color(UIColor.secondarySystemFill))
-                            }
-                        )
-                        .overlay {
-                            switch size {
-                            case .extraLarge:
-                                avatarShape.stroke(.background, lineWidth: 2)
-                            default:
-                                avatarShape.stroke(.separator, lineWidth: 0.3)
-                            }
-                        }
-                    } else {
-                        avatarShape
-                            .foregroundStyle(
-                                Color(UIColor.secondarySystemFill))
-                    }
-                case .local(let image):
-                    image.resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .clipShape(avatarShape)
-                        .overlay {
-                            switch size {
-                            case .extraLarge:
-                                avatarShape.stroke(.background, lineWidth: 2)
-                            default:
-                                avatarShape.stroke(.separator, lineWidth: 0.3)
-                            }
-                        }
+            avatarImageOrPlaceholder
+                .background() {
+                    avatarShape.fill(.background) // in case the avatar has an alpha channel
                 }
-            }
+                .overlay {
+                    switch borderStyle {
+                    case .backgroundMatching:
+                        avatarShape.stroke(.background, lineWidth: 2)
+                    case .separator:
+                        avatarShape.stroke(.separator, lineWidth: 1 / displayScale)
+                    case .both:
+                        ZStack {
+                            avatarShape
+                                .stroke(.background, lineWidth: 2)
+                            avatarShape
+                                .inset(by: 1)
+                                .strokeBorder(.separator, lineWidth: 1 / displayScale)
+                        }
+                    case .none:
+                        switch size {
+                        case .extraLarge:
+                            avatarShape.stroke(.background, lineWidth: 2)
+                        default:
+                            avatarShape.stroke(.separator, lineWidth: 1 / displayScale)
+                        }
+                    }
+                }
             
             if isNavigating {
                 ProgressView()
@@ -144,6 +136,38 @@ public struct AvatarView: View {
                     }
                     isNavigating = false
                 }
+            }
+        }
+    }
+    
+    @ViewBuilder var avatarImageOrPlaceholder: some View {
+        if let avatarSource {
+            switch avatarSource {
+            case .url(let url):
+                if let url {
+                    WebImage(
+                        url: url,
+                        content: { image in
+                            image.resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .clipShape(avatarShape)
+                        },
+                        placeholder: {
+                            avatarShape
+                                .foregroundStyle(
+                                    Color(UIColor.secondarySystemFill))
+                        }
+                    )
+                  
+                } else {
+                    avatarShape
+                        .foregroundStyle(
+                            Color(UIColor.secondarySystemFill))
+                }
+            case .local(let image):
+                image.resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(avatarShape)
             }
         }
     }
