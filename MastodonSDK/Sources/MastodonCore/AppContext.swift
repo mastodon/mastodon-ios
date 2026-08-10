e2bfb80f17a6b6d2f0614934e08927a250b70a46
shannon <shannon@joinmastodon.org>
2026-08-10 14:36:47 -0400

    Remove obsolete code after migrating About Mastodon view

--- MastodonSDK/Sources/MastodonCore/AppContext.swift
+++ MastodonSDK/Sources/MastodonCore/AppContext.swift
@@ -8,7 +8,6 @@
 import UIKit
 import SwiftUI
 import Combine
-import AlamofireImage
 
 @MainActor
 public class AppContext: ObservableObject {
@@ -26,63 +25,5 @@ public class AppContext: ObservableObject {
         .eraseToAnyPublisher()
     
     private init() {
-    }
-}
-
-extension AppContext {
-    
-    public typealias ByteCount = Int
-    
-    public static let byteCountFormatter: ByteCountFormatter = {
-        let formatter = ByteCountFormatter()
-        return formatter
-    }()
-    
-    public func purgeCache() {
-        ImageDownloader.defaultURLCache().removeAllCachedResponses()
-
-        let fileManager = FileManager.default
-        let temporaryDirectoryURL = fileManager.temporaryDirectory
-        let fileKeys: [URLResourceKey] = [.fileSizeKey, .isDirectoryKey]
-
-        if let directoryEnumerator = fileManager.enumerator(
-            at: temporaryDirectoryURL,
-            includingPropertiesForKeys: fileKeys,
-            options: .skipsHiddenFiles) {
-            for case let fileURL as URL in directoryEnumerator {
-                guard let resourceValues = try? fileURL.resourceValues(forKeys: Set(fileKeys)),
-                      resourceValues.isDirectory == false else {
-                    continue
-                }
-
-                try? fileManager.removeItem(at: fileURL)
-            }
-        }
-    }
-
-    // In Bytes
-    public func currentDiskUsage() -> Int {
-        let alamoFireDiskBytes = ImageDownloader.defaultURLCache().currentDiskUsage
-
-        var tempFilesDiskBytes = 0
-        let fileManager = FileManager.default
-        let temporaryDirectoryURL = fileManager.temporaryDirectory
-        let fileKeys: [URLResourceKey] = [.fileSizeKey, .isDirectoryKey]
-
-        if let directoryEnumerator = fileManager.enumerator(
-            at: temporaryDirectoryURL,
-            includingPropertiesForKeys: fileKeys,
-            options: .skipsHiddenFiles) {
-            for case let fileURL as URL in directoryEnumerator {
-                guard let resourceValues = try? fileURL.resourceValues(forKeys: Set(fileKeys)),
-                      resourceValues.isDirectory == false else {
-                    continue
-                }
-
-                tempFilesDiskBytes += resourceValues.fileSize ?? 0
-            }
-        }
-
-        return alamoFireDiskBytes + tempFilesDiskBytes
     }
 }
